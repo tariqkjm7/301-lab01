689e6611aa475d3a23b98e15cd86e8f7574a1564
tariqkjm7 <d.alatoum@gmail.com>
2021-09-13 13:29:10 +0300

    anything

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ render(){
 
             <div>
 
-           <HornedBeast title = {'UniWhal'} img = {"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} 
+           <HornedBeast title = {'UniWhal'} img = {"https://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} 
            description = {'Mother (or father) rhino with two babies'} />
            
            <HornedBeast title = {'Unicorn Head'} img = {"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"} 
