cce0d6d9e634c549795da8a01383c58bb9570a92
tariqkjm7 <d.alatoum@gmail.com>
2021-09-13 15:51:02 +0300

    all done

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,10 +1,43 @@
 import React from "react";
-class HornedBeast extends React.Component{
-    render(){
-        return(
-           <>
-          <h2>{this.props.title}</h2> , <img src={this.props.img} alt="" srcset="" /> , <p>{this.props.description}</p> 
-          </>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import './card.css';
+import Col from "react-bootstrap/Col";
+
+class HornedBeast extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfClick: 0
+        }
+    }
+
+    increaseNumberOfclicks = () => {
+
+        this.setState({
+            numOfClick: this.state.numOfClick + 1
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <Col>
+                <Card className='card' style={{ width: '18rem' }} >
+                    <Card.Img  className='cardi'variant="top" src={this.props.img} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            number of clicks {this.state.numOfClick}
+                        </Card.Text>
+                        <Button  className='cardB' onClick={this.increaseNumberOfclicks} variant="primary">Vote </Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+
+            </div>
         )
     }
 
