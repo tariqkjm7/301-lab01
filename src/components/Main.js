67b538b186b58ab7fdadbbf2d8151a71c4ad9d12
tariqkjm7 <d.alatoum@gmail.com>
2021-09-13 13:36:10 +0300

    anything

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HornedBeast from './HornedBeast';
+import data from './data.json'
 
 class Main extends React.Component{
 render(){
@@ -8,7 +9,7 @@ render(){
 
             <div>
 
-           <HornedBeast title = {'UniWhal'} img = {"https://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} 
+           <HornedBeast title = {'UniWhal'} img = {"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"} 
            description = {'Mother (or father) rhino with two babies'} />
            
            <HornedBeast title = {'Unicorn Head'} img = {"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"} 
